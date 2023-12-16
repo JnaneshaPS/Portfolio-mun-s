b012e61ec30398c60382f57b95f658e1dc8d9be7
Jnanesh <jnaneshps4@gmail.com>
2023-12-16 12:48:00 +0530

    seventh commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clint | Front-End Developer</title>
+        <title>Jnanesha| Full-stack Developer</title>
         <meta name="description" content="I’m a full-stack web developer specializing in building (and occasionally designing) exceptional digital experiences." />
         <link rel="icon" href="/fav.png" />
       </Head>
