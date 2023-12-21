49850c6ba48a95ec57881af55694eb2ffafada81
Jnanesh <jnaneshps4@gmail.com>
2023-12-21 14:35:11 +0530

    thirtenth commit

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   return (
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+        <p className='text-xl tracking-widest uppercase text-[#000000]'>
           Contact
         </p>
         <h2 className='py-4'>Get In Touch</h2>
